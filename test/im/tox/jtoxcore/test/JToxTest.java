e558d47709bf4a5296e1907c18cca012222f36a2
Simon Levermann <simon@levermaenner.de>
2013-09-02 17:42:56 +0200

    Test client
    Add a basic working test client that will print its ID and autoconfirm received friend requests. On receiving
    messages, the message including the local friend ID is printed

--- test/im/tox/jtoxcore/test/JToxTest.java
+++ test/im/tox/jtoxcore/test/JToxTest.java
@@ -5,34 +5,24 @@ import java.net.Inet4Address;
 import java.net.UnknownHostException;
 
 import im.tox.jtoxcore.JTox;
+import im.tox.jtoxcore.ToxError;
 import im.tox.jtoxcore.ToxException;
 import im.tox.jtoxcore.callbacks.OnFriendRequestCallback;
+import im.tox.jtoxcore.callbacks.OnMessageCallback;
 
 public class JToxTest {
 	public static void main(String[] args) {
-		System.out.println(System.getProperty("java.library.path"));
 		try {
-			JTox jtox = JTox.newTox();
+			JTox jtox = new JTox();
+			System.out.println(jtox.getAddress());
 			jtox.bootstrap(
 					new InetSocketAddress(Inet4Address
-							.getByName("198.46.136.167"), 33445),
-					"728925473812C7AAC482BE7250BCCAD0B8CB9F737BF3D42ABD34459C1768F854");
-			String addr = jtox.getAddress();
-			jtox.setOnFriendRequestCallback(new OnFriendRequestCallback() {
-
-				@Override
-				public void execute(String publicKey, String message) {
-					System.out
-							.println("Yay! We received a friend request from: "
-									+ publicKey
-									+ " with "
-									+ (message.isEmpty() ? "no message."
-											: "the following message: "
-													+ message));
-				}
-			});
-			System.out.println(addr);
-			while (true) {
+							.getByName("54.215.145.71"), 33445),
+					"6EDDEE2188EF579303C0766B4796DCBA89C93058B6032FEA51593DCD42FB746C");
+			jtox.setOnFriendRequestCallback(new MyOnFriendRequestCallback(jtox));
+			jtox.setOnMessageCallback(new MyOnMessageCallback());
+			
+			while(true) {
 				jtox.doTox();
 			}
 		} catch (ToxException e) {
@@ -43,4 +33,37 @@ public class JToxTest {
 			e.printStackTrace();
 		}
 	}
+}
+
+class MyOnFriendRequestCallback extends OnFriendRequestCallback {
+	private JTox jtox;
+
+	public MyOnFriendRequestCallback(JTox jtox) {
+		this.jtox = jtox;
+	}
+
+	@Override
+	public void execute(String publicKey, String message) {
+		System.out.println("Yay! We received a friend request from: "
+				+ publicKey
+				+ " with "
+				+ (message.isEmpty() ? "no message."
+						: "the following message: " + message));
+		try {
+			this.jtox.confirmRequest(publicKey);
+			System.out.println("Friend request has been autoconfirmed!");
+		} catch (ToxException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+}
+
+class MyOnMessageCallback extends OnMessageCallback {
+
+	@Override
+	public void execute(int friendNumber, String message) {
+		System.out.println("Message from " + friendNumber + ": " + message);
+	}
+
 }
